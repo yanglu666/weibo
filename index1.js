9f6f91441c60f38d0f6d0859606c7b2b2959801d
yanglu666 <48917030+yanglu666@users.noreply.github.com>
2019-04-25 22:12:38 +0800

    提交作业

--- index1.js
+++ index1.js
@@ -1,8 +1,8 @@
 let dropdownIsOpen = false;
 let dropdownElement = null;
 let dropdownInput = document.querySelectorAll(".dropdown .input");
-dropdownInput.forEach(function(item){
-    item.addEventListener("click",function(e){
+dropdownInput.forEach(function (item) {
+    item.addEventListener("click", function (e) {
         const ul = e.target.nextElementSibling;
         ul.style.display = "block";
         dropdownElement = ul;
@@ -10,20 +10,20 @@ dropdownInput.forEach(function(item){
     });
 });
 
-window.addEventListener("click",function(){
-if(dropdownIsOpen){
-    dropdownElement.style.display = "none";
-    dropdownElement = null;
-    dropdownIsOpen = false;
-}
-},true);
+window.addEventListener("click", function () {
+    if (dropdownIsOpen) {
+        dropdownElement.style.display = "none";
+        dropdownElement = null;
+        dropdownIsOpen = false;
+    }
+}, true);
 
 
 let dropdownIsOpen1 = false;
 let dropdownElement1 = null;
 let dropdownInput1 = document.querySelectorAll(".dropdown1 .btn2");
-dropdownInput1.forEach(function(item){
-    item.addEventListener("click",function(e){
+dropdownInput1.forEach(function (item) {
+    item.addEventListener("click", function (e) {
         const ul = e.target.nextElementSibling;
         ul.style.display = "block";
         dropdownElement1 = ul;
@@ -31,20 +31,20 @@ dropdownInput1.forEach(function(item){
     });
 });
 
-window.addEventListener("click",function(){
-if(dropdownIsOpen1){
-    dropdownElement1.style.display = "none";
-    dropdownElement1 = null;
-    dropdownIsOpen1 = false;
-}
-},true);
+window.addEventListener("click", function () {
+    if (dropdownIsOpen1) {
+        dropdownElement1.style.display = "none";
+        dropdownElement1 = null;
+        dropdownIsOpen1 = false;
+    }
+}, true);
 
 
 let dropdownIsOpen2 = false;
 let dropdownElement2 = null;
 let dropdownInput2 = document.querySelectorAll(".dropdown2 .btn4");
-dropdownInput2.forEach(function(item){
-    item.addEventListener("click",function(e){
+dropdownInput2.forEach(function (item) {
+    item.addEventListener("click", function (e) {
         const ul = e.target.nextElementSibling;
         ul.style.display = "block";
         dropdownElement2 = ul;
@@ -52,10 +52,61 @@ dropdownInput2.forEach(function(item){
     });
 });
 
-window.addEventListener("click",function(){
-if(dropdownIsOpen2){
-    dropdownElement2.style.display = "none";
-    dropdownElement2 = null;
-    dropdownIsOpen2 = false;
+window.addEventListener("click", function () {
+    if (dropdownIsOpen2) {
+        dropdownElement2.style.display = "none";
+        dropdownElement2 = null;
+        dropdownIsOpen2 = false;
+    }
+}, true);
+
+
+
+function isELInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
 }
-},true);
+let loadStatus = false;
+window.addEventListener("scroll", () => {
+    if(loadStatus){
+        return;
+    }
+    const loadmore = document.getElementById("loadmore");
+    //如果div在可见区域，执行加载其他页内容
+    if (isELInViewport(loadmore)) {
+        loadMore();
+    }
+        handleVideo();
+});
+
+function loadMore() {
+    
+        loadStatus = true;
+        setTimeout(() => {
+            
+                let li = document.createElement('li');
+                li.innerHTML = '迪丽热巴';
+                document.getElementById("list").appendChild(li);
+            loadStatus = false;   
+    }, 1000);
+}
+
+ function  handleVideo(){
+    let video = document.querySelector('.myvideo');
+    if (isELInViewport(video)) {
+        if (video.paused) {
+         //   video.muted = true;
+            video.play();
+        }
+    }else{
+        if (!video.paused){
+            video.pause();
+        }
+    }
+ }
+
+  
+
+
+
+
